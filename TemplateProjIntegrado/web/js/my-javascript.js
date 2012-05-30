b9baae1562339d2d22654024a7ee79bc0f129480
nahimsouza@yahoo.com.br <nahimsouza@yahoo.com.br@f304061d-1f21-a059-09c6-ef40fbe8d06b>
2012-05-30 16:34:29 +0000

    Projeto com novo template

---
+++ TemplateProjIntegrado/web/js/my-javascript.js
@@ -0,0 +1,30 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function showDiv( idName, value ){
+    //funcao ShowDiv retirada de: http://www.guj.com.br/java/136558-mudar-conteudo-de-uma-div-resolvido 
+    objDiv = document.getElementById( idName );
+    if( value )
+        objDiv.style.display = "";
+    else
+        objDiv.style.display = "none";
+}
+
+function abrirPag(valor){
+    var url = valor;
+
+    xmlRequest.onreadystatechange = mudancaEstado;
+    xmlRequest.open("GET",url,true);
+    xmlRequest.send(null);
+    
+    return url;
+}
+
+function mudancaEstado(){
+    if (xmlRequest.readyState == 4){
+        document.getElementById("conteudo").innerHTML = xmlRequest.responseText;
+    }
+}
